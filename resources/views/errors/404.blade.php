25b4832eb0a027c46364b821765e12fe2d58ef52
tillo <tomas.illo@lopuch.sk>
2019-04-05 09:47:56 +0200

    errors setup

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ URL::asset('css/error.css') }}">
+@stop
+
+@section('content')
+	<h1>Stránka nenájdená.</h1>
+@stop
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ URL::asset('css/error.css') }}">
+@stop
+
+@section('content')
+	<h1>Prebieha údržba servera.</h1>
+@stop
